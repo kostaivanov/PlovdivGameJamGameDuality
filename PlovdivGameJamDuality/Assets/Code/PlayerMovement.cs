85352c46ed3e9092cc9f699e8fcf170471187787
kostaivanov <kostasvetlozarov@gmail.com>
2022-01-27 22:28:29 +0200

    Update PlayerMovement.cs and SampleScene.unity

--- PlovdivGameJamDuality/Assets/Code/PlayerMovement.cs
+++ PlovdivGameJamDuality/Assets/Code/PlayerMovement.cs
@@ -7,13 +7,14 @@ internal class PlayerMovement : ObjectComponents, IMovable
 {
     private float move_X, move_Y;
     private Vector2 moveDirection;
-
-
+    [SerializeField] private float speed;
+    private bool isMoving;
 
     // Start is called before the first frame update
     protected override void Start()
     {
         base.Start();
+        isMoving = false;
     }
 
     // Update is called once per frame
@@ -25,16 +26,25 @@ internal class PlayerMovement : ObjectComponents, IMovable
         }
     }
 
+
+    private void FixedUpdate()
+    {
+        if (isMoving == true)
+        {
+            Move();
+        }   
+    }
+
     private void ProcessInput()
     {
         //move_X = Input.GetAxisRaw("Horizontal");
         move_Y = Input.GetAxisRaw("Vertical");
-        moveDirection = new Vector2(move_X, move_Y).normalized;
-
+        moveDirection = new Vector2(0, move_Y).normalized;
+        isMoving = true;
     }
 
     public void Move()
     {
-        rigidBody.velocity = new Vector2(rigidBody.velocity.x, moveDirection.y * moveSpeed * dash * Time.fixedDeltaTime);
+        rigidBody.velocity = new Vector2(rigidBody.velocity.x, moveDirection.y * speed  * Time.fixedDeltaTime);
     }
 }
